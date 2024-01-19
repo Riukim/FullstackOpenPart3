a722955417c0e4cbfcad34a478ae39ae00d70229
Riukim <m.cominelli29@gmail.com>
2024-01-19 09:30:55 +0100

    Added POST and generateId

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,66 +1,92 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 let persons = [
-    { 
-      id: 1,
-      name: "Arto Hellas", 
-      number: "040-123456"
-    },
-    { 
-      id: 2,
-      name: "Ada Lovelace", 
-      number: "39-44-5323523"
-    },
-    { 
-      id: 3,
-      name: "Dan Abramov", 
-      number: "12-43-234345"
-    },
-    { 
-      id: 4,
-      name: "Mary Poppendieck", 
-      number: "39-23-6423122"
-    },
-    { 
-        id: 5,
-        name: "Gino Gini", 
-        number: "39-23-6423122"
-      }
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+  {
+    id: 5,
+    name: "Gino Gini",
+    number: "39-23-6423122",
+  },
+];
 
+app.get("/info", (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people!</p>
+    <p>${Date()}</p>`);
+});
 
-app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${persons.length} people!</p>
-    <p>${Date()}</p>`)
-})
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
-})
+const generateId = () => {
+  const randomId = Math.floor(Math.random() * 1000000);
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-  
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  
-})
+  const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
 
-app.delete('/api/persons/:id', (request, response) =>{
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+  return Math.max(maxId, randomId) + 1;
+};
 
-    response.status(204).end()
-})
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
 
-const PORT = 3001
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
